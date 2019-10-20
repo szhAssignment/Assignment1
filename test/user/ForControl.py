fc5394bec40f9a97a7dc3a244f00eb143b1d2010
szhUser <szh267270@hotmail.com>
2019-10-20 00:22:58 -0700

    second commit

---
+++ test/user/ForControl.py
@@ -0,0 +1,4 @@
+import random
+
+for i in range(1,random.randint(5,15)):
+    print("This is  for loop has already run" + str(i)+"times.")
---
+++ test/user/IfControl.py
@@ -0,0 +1,8 @@
+print("How old are you?")
+age = int(input())
+if age < 22:
+    print("you are too young to have a drink.")
+elif age >=80:
+    print("Ok, you will get a free drink.")
+else:
+    print("Sure, enjoy your drink")
